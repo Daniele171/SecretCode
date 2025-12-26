99af4da423e4b0125d5dced1aa5bd9be1828528f
Daniele-dev <156462558+Daniele171@users.noreply.github.com>
2025-12-26 11:24:22 +0100

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const COLORS = ['orange', 'yellow', 'red', 'green', 'blue', 'brown'];
+export const CODE_LENGTH = 4;
+export const MAX_ROWS = 10;
+export const CSS_COLOR_MAP = {
+    'orange': '#FFA500',
+    'yellow': '#FFD700',
+    'red':    '#FF4136',
+    'green':  '#2ECC40',
+    'blue':   '#0074D9',
+    'brown':  '#8B4513',
+    'empty':  '#222'
+};
